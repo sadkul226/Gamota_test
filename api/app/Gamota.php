3fa76817fce389aa554a55ddb23c1e85afe1187d
Nguyen Xuan Cuong <xuancuong220691@gmail.com>
2016-04-30 17:05:10 +0700

    add api get users

--- api/app/Gamota.php
+++ api/app/Gamota.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Gamota extends Model
 {
-    //
+
+    protected $table = 'inapp_card_sandbox';
+
 }
--- api/app/Http/Controllers/GamotaController.php
+++ api/app/Http/Controllers/GamotaController.php
@@ -2,11 +2,46 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
-use App\Http\Requests;
+use App\Gamota;
+use Illuminate\Support\Facades\Input;
 
 class GamotaController extends Controller
 {
-    //
+    // Defile constant status with value success
+    const STATUS_SUCCESS = 'success';
+
+    /**
+     * Funtion get users most charge
+     * 
+     * @return list users
+     */
+    public function get()
+    {
+        $users  = collect();
+        $errors = [];
+        $query  = Gamota::where('status', self::STATUS_SUCCESS)
+            ->where('target', '!=', '')
+            ->groupBy('target')
+            ->selectRaw('sum(amount) as total_amount, target, status, request_time, error_code')
+            ->orderBy('total_amount', 'DESC');
+        if (Input::has('limit')) {
+            if (filter_input(INPUT_GET, 'limit', FILTER_VALIDATE_INT)) {
+                $query->limit(Input::get('limit'));
+                $errors['error_code'] = 0;
+                $users                = $query->get();
+            } else {
+                $errors['error_code'] = 1;
+                $errors['error_msg']  = 'Param limit is not interger type';
+            }
+        } else {
+            $errors['error_code'] = 0;
+            $users                = $query->get();
+        }
+
+        return \Response::json([
+                'errors' => json_encode($errors),
+                'data'   => $users->toJson()
+        ]);
+    }
+
 }
